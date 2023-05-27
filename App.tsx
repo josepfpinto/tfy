c89d927454a88d640fa5d46256cfcf988016c67a
josepfpinto <josepfpinto@gmail.com>
2023-05-28 00:12:30 +0100

    frontend dependencies

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import awsExports from './src/aws-exports';
 Amplify.configure(awsExports);
 
 export default function App() {
-  console.log('starting app!');
   return (
     <View style={styles.container}>
       <Text>This is my new app!</Text>
